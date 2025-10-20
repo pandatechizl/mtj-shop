e166cfa3e12f698a80aa8f4b3001f3478ee66968
inzali <shweyamininzali@gmail.com>
2025-10-21 03:51:02 +0700

    Update image urls.

--- js/products.js
+++ js/products.js
@@ -5,27 +5,27 @@ const products = [
     name: "Gold Charm Necklace",
     price: 22.9,
     description: "Chic gold necklace with leaf charm",
-    imageUrl: "https://drive.usercontent.google.com/download?id=13fhBwJGOhNQYLm7AKCjrmE3S0mNbF5mL&export=view"
+    imageUrl: "https://drive.google.com/uc?export=view&id==13fhBwJGOhNQYLm7AKCjrmE3S0mNbF5mL"
   },
   {
     id: 2,
     name: "Minimalist Bracelet",
     price: 14.5,
     description: "Simple bracelet for daily wear",
-    imageUrl: "https://drive.usercontent.google.com/download?id=13fhBwJGOhNQYLm7AKCjrmE3S0mNbF5mL&export=view"
+    imageUrl: "https://drive.google.com/uc?export=view&id==13fhBwJGOhNQYLm7AKCjrmE3S0mNbF5mL"
   },
   {
     id: 3,
     name: "Pearl Drop Earrings",
     price: 18.0,
     description: "Elegant pearl drop earrings",
-    imageUrl: "https://drive.usercontent.google.com/download?id=113fhBwJGOhNQYLm7AKCjrmE3S0mNbF5mL&export=view"
+    imageUrl: "https://drive.google.com/uc?export=view&id==113fhBwJGOhNQYLm7AKCjrmE3S0mNbF5mL"
   },
   {
     id: 4,
     name: "Stackable Rings Set",
     price: 16.9,
     description: "Gold-plated rings set",
-    imageUrl: "https://drive.usercontent.google.com/download?id=13JZM7WGn-QId5vOkSlO-1JK2XDAVF2H2&export=view"
+    imageUrl: "https://drive.google.com/uc?export=view&id==13JZM7WGn-QId5vOkSlO-1JK2XDAVF2H2"
   }
 ];
